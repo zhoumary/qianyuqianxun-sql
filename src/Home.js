d80105381681819a27aa2132da0243c7ee0b7cd1
I323799 <fiona.zhou01@sap.com>
2020-02-23 22:30:31 +0800

    add Home Page

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import './Home.css';
 // import Footer from './Footer';
+import Logo from './pics/logo01.gif';
+import Daocheng from './pics/daocheng.jpg';
+import Linzhi from './pics/linzhi.jpeg';
+import Xihu from './pics/xihu.jpg';
+import Miaozhai from './pics/miaozhai.png';
+import Hulunbeier from './pics/hunlunbeier.jpg';
 
 
 class ToolBar extends React.Component {
@@ -10,9 +16,57 @@ class ToolBar extends React.Component {
 
 
         return (
-            <div>
-                Qian Yu Qian Xun
-            </div>
+            <>
+                {/* <header>
+                    <a href="#" id="logo"><img src={Logo} /></a>
+                    <nav>
+                        <a href="#" id="menu-icon"></a>
+                        <ul>
+                            <li><a href="#" class="current">首頁</a></li>
+                            <li><a href="#">目的地</a></li>
+                            <li><a href="#">攻略</a></li>
+                            <li><a href="#">聯係我們</a></li>
+                        </ul>
+                    </nav>
+                </header> */}
+
+                <header>
+                    <a href="#" id="homeLink"><img src={Logo} /></a>
+                    <nav>
+                        <ul id="navList">
+                            <li><a href="#">首頁</a></li>
+                            <li><a href="#">目的地</a></li>
+                            <li><a href="#">旅游攻略</a></li>
+                            <li><a href="#">聯係我們</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <div id="focus">
+                    <div id="textSearch">
+                        <div id="description">
+                            <div>
+                                Sciences Description
+                            </div>
+                        </div>
+
+                        <div id="searchBox">
+                            <form>
+                                <input type="text" placeholder="Search.." name="search" />
+                                <button type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div id="placeList">
+                        <ul id="imageList">
+                            <li><img src={Daocheng} /></li>
+                            <li><img src={Linzhi} /></li>
+                            <li><img src={Xihu} /></li>
+                            <li><img src={Miaozhai} /></li>
+                            <li><img src={Hulunbeier} /></li>
+                        </ul>
+                    </div>
+                </div>
+            </>
         )
     }
 }
@@ -21,7 +75,7 @@ class RecommList extends React.Component {
     render() {
         return (
             <div>
-
+                Recommended List
             </div>
         )
     }
@@ -47,9 +101,9 @@ class Home extends React.Component {
                     <div id="recommList">
                         <RecommList />
                     </div>
-                    {/* <div id="footer">
-                        <Footer />
-                    </div> */}
+                    <div id="footer">
+                        {/* <Footer /> */}
+                    </div>
                 </div>
             </Router>
         );
